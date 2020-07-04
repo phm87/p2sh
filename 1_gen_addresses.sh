c41371b1d0391fafb282a95bb8b55a76f379bd62
phm87 <31578435+phm87@users.noreply.github.com>
2020-07-04 14:05:04 +0200

    Update 1_gen_addresses.sh

--- 1_gen_addresses.sh
+++ 1_gen_addresses.sh
@@ -14,3 +14,12 @@ echo "Address: "$alice_address
 alice_validateaddress=$(./komodo-cli validateaddress $alice_address)
 alice_pubkey=$(echo $alice_validateaddress | jq -r '.pubkey')
 echo "Pubkey: "$alice_pubkey
+
+# TODO: Check that config file doesn't exist
+
+echo "bob_address="$bob_address >> config
+echo "bob_pubkey="$bob_pubkey >> config
+echo "alice_address="$alice_address >> config
+echo "alice_pubkey="$alice_pubkey >> config
+
+echo "Addresses generated, please run the Script 2 to generate script and P2SH address"
