dbbf558197b576d9125e5ffe09bac698ca4c5e72
phm87 <31578435+phm87@users.noreply.github.com>
2020-07-04 13:26:14 +0200

    Update 1_gen_addresses.sh

--- 1_gen_addresses.sh
+++ 1_gen_addresses.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 echo "P2SH Example"
 
+echo "---> Bob:"
+bob_address=$(./komodo-cli getnewaddress)
+echo "Address: "$bob_address
+bob_validateaddress=$(./komodo-cli validateaddress $bob_address)
+bob_pubkey=$(echo $bob_validateaddress | jq -r '.pubkey')
+echo "Pubkey: "$bob_pubkey
+
+echo "---> Alice:"
+alice_address=$(./komodo-cli getnewaddress)
+echo "Address: "$alice_address
+alice_validateaddress=$(./komodo-cli validateaddress $alice_address)
+alice_pubkey=$(echo $alice_validateaddress | jq -r '.pubkey')
+echo "Pubkey: "$alice_pubkey
